eea04cffa2d3b1feffa513ddaccf3057d1ceb192
vova77 <vova2000smi@gmail.com>
2023-10-25 18:23:21 +0300

    Created layout

--- app/src/main/java/com/example/todoapplication/domain/GetToDoListUseCase.kt
+++ app/src/main/java/com/example/todoapplication/domain/GetToDoListUseCase.kt
@@ -1,8 +1,10 @@
 package com.example.todoapplication.domain
 
+import androidx.lifecycle.LiveData
+
 class GetToDoListUseCase(private val toDoListRepository: ToDoListRepository) {
 
-    fun getToDoList(): List<ToDoItem> {
-       return toDoListRepository.getToDoList()
+    fun getToDoList(): LiveData<List<ToDoItem>> {
+        return toDoListRepository.getToDoList()
     }
 }
