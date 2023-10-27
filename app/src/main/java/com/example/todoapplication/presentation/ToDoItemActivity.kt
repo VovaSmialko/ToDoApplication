325d87ec95afd24eda35ac098248298a056232b1
vova77 <vova2000smi@gmail.com>
2023-10-27 22:45:46 +0300

    Created RecyclerView

---
+++ app/src/main/java/com/example/todoapplication/presentation/ToDoItemActivity.kt
@@ -0,0 +1,12 @@
+package com.example.todoapplication.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.todoapplication.R
+
+class ToDoItemActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_to_do_item)
+    }
+}
